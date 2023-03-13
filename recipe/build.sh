10742e879657683bf781d915b507794310ee0ebd
sgupta1524 <sgupta@asrtinc.com>
2023-03-13 19:44:18 -0400

    building conda package; import tested; TODO: add entrypoint

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-chmod +x pastrami.py
+#chmod +x $SRC_DIR/pastrami.py
+$PYTHON setup.py install --single-version-externally-managed --record=record.txt
+$PYTHON -m pip install --no-deps .  
+PIP_NO_CACHE_DIR=True
+$PYTHON -m pip install --no-deps . 
